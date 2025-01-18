8075fec34c2dc69fce8a39708b3757d3e5df964c
Divyansh Bansal <bansal9divyansh@gmail.com>
2025-01-18 19:04:35 +0530

    deprecated options

--- pdfFileUploadCloudinary/server.js
+++ pdfFileUploadCloudinary/server.js
@@ -28,7 +28,7 @@ const PdfModel = mongoose.model('Pdf', pdfSchema);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
